b6718a6fbc3019cb4426d45f2f39a8f124dce2dd
mukunddp <mukundparve98@gmail.com>
2023-10-10 08:16:14 +0530

    Initial commit

---
+++ firstfile.py
@@ -0,0 +1,6 @@
+print('Python')                 # 0.1 s
+print("Mukund Parve")           # 2s
+print("Mukund Parve..")         # 3s
+print("Mukund Parve ...")       # 1s
+for i in range(1, 5):
+    print("I am for loop")
