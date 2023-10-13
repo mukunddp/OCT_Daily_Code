d25471a2dfb7b362015491d90ae58e8fd5223627
mukunddp <mukundparve98@gmail.com>
2023-10-13 08:52:02 +0530

    updated

---
+++ 5_string_methods.py
@@ -0,0 +1,20 @@
+# string.method()
+
+# print(String_variable.count('l'))
+# print(String_variable.upper())
+# print(String_variable.lower())
+# print(String_variable.title())
+# print(String_variable.capitalize())
+
+String_variable = "I am {name}, I am learning Python 27" # 0, 1, 2, 3 ....
+# name = "Mukund"
+# f_String = f"I am {name}, I am learning Python"
+
+# print(String_variable.format(name="Mukund"))
+# print(String_variable.index('P'))
+# print(String_variable.find('M'))
+s = "          18Mukund    "
+# print(s.isalnum())
+# print(s.isalpha())
+print(s)
+print(s.strip())
---
+++ 6_slicing_string.py
@@ -0,0 +1,11 @@
+# string = "Tech Amplifiers"
+# print(string.index(' '))
+#
+# first_name = string[0:string.index(' ')]
+# print(first_name)
+# last_name = string[string.index(' ')+1:]
+# print(last_name)
+
+# my_email = 'myname@techamplifiers.com'
+# username = ?
+# domain_name = ?
